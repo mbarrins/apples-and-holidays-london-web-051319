761029cee9a84192fb1f83484022693bd62d0da7
Maija Barrins <maija.barrins@gmail.com>
2019-04-27 19:32:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,9 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize}:"
-    holidays.each {|holiday, supplies| puts "  #{holiday.so_s.capitalize}: #{supplies.join(", ")}"}
+    holidays.each do |holiday, supplies|
+      puts "  #{holiday.so_s.capitalize}: #{supplies.join(", ")}"
+    end
   end
 end
 
